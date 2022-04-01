d2ddbf617569d0d5d823048057e6bea9d3f6fad6
treissen <timreissenart@gmail.com>
2022-03-31 20:45:22 -0600

    edit js

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 const subtract = (a, b) => {
     return a - b;
 }
-
-
 const add = (a, b) => {
     return a + b;
 }
-
-
 const divide = (a, b) => {
     return a / b;
 }
-
-
 const multiply = (a, b) => {
     return a * b;
 }
 
 
-
-const operate = ( /* user inputs */) => {
+const operate = (opertor, a, b) => {
 
 }
