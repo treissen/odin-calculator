54c856668ae043f5f035ce23d2e481ed9d75bdfc
treissen <timreissenart@gmail.com>
2022-04-01 09:05:23 -0600

    added onlcick to display in html the value of btn

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let currentInput = '' // what the user just clicked
 
 
 
-// on button click assign value to current input
-function numberClickHandler(value) {
+// on button click assign value to current input and write to html
+function calcClickHandler(value) {
     currentInput = value;
-    document.getElementById('inputDisplay').innerHTML = currentInput;
+    document.getElementById('inputDisplay').innerHTML = value.toString();
 }
 
 
